fde822a0e324e0bbefc791c6dd55b0f73278e12b
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-28 14:27:41 +0200

    hardcoded landing page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 export const Home = () => {
-    return (
-        <div>
-        <h1>Home Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>TITEL PÅ TJÄNSTEN</h1>
+      <h1>RUTA MED Subscription 1</h1>
+      <h1>RUTA MED Subscription 2</h1>
+      <h1>RUTA MED Subscription 3</h1>
+    </div>
+  );
+};

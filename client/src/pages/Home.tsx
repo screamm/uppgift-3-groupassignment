dd9eca796522dbaa78022fe4507278238ac32456
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-28 14:37:36 +0200

    structure

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,12 @@ export const Home = () => {
   return (
     <div>
       <h1>TITEL PÅ TJÄNSTEN</h1>
-      <h1>RUTA MED Subscription 1</h1>
-      <h1>RUTA MED Subscription 2</h1>
-      <h1>RUTA MED Subscription 3</h1>
+      <h1>RUTA MED Subscription Level 1</h1>
+      <button>KÖP</button>
+      <h1>RUTA MED Subscription Level 2</h1>
+      <button>KÖP</button>
+      <h1>RUTA MED Subscription Level 3</h1>
+      <button>KÖP</button>
     </div>
   );
 };

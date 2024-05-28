3eb567e5a5ec570aa35da8f7bc49496b2c5b84ec
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-28 17:03:13 +0200

    added contact page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { NotFound } from "./pages/NotFound"; 
+import { NotFound } from "./pages/NotFound";
 import { Home } from "./pages/Home";
 import { MyPages } from "./pages/MyPages";
 import { Layout } from "./pages/Layout";
 import { Admin } from "./pages/Admin";
 import { Login } from "./pages/Login";
+import { Contact } from "./pages/Contact";
 
 export const router = createBrowserRouter([
   {
@@ -23,13 +24,17 @@ export const router = createBrowserRouter([
         element: <Login />,
       },
       {
-      path: "/mypages",
-       element: <MyPages />
-       },
-       {
+        path: "/mypages",
+        element: <MyPages />,
+      },
+      {
         path: "/admin",
-         element: <Admin />
-         },
+        element: <Admin />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
     ],
   },
 ]);
---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,7 @@
+export const Contact = () => {
+  return (
+    <div>
+      <h1>Kontaktuppgifter Tjoho ring oss</h1>
+    </div>
+  );
+};

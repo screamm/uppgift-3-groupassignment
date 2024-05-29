053a9a419be2a0ce173c81115844111a62218f6f
fabian0270 <fabianolsson28@gmail.com>
2024-05-29 02:10:30 +0200

    Just created the outline of admin. No functionality yet.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,9 +1,41 @@
+import "../styles/admin.css"
+
 export const Admin = () => {
     return(
-        <div>
-        <h1>Admin</h1>
-        <h3>En administratör ska kunna lägga in innehållssidor och välja vilken nivå man måste ha för att få se den</h3>
+        <div className="admin">
+            <h2>Produktlista</h2>
+            <div>
+                    <div>
+                        <h6>Artikelnamn: </h6>
+                        <input
+                            type="text"
+                     />
+                        <label>Pris: </label>
+                        <input
+                           type="text"
+                     />
+                        <button>Spara</button>
+                    </div>
+                    <div>
+                        <h1>Prenumerationer</h1>
+                         <p>Artikelpris:  kr</p>
+                         <button>Redigera Prenumeration</button>
+                    </div>
+            </div>
+            <div>
+                <label>Artikelnamn: </label>
+                    <input
+                         type="text"
+                    />
+                    <label>Pris: </label>
+                    <input
+                     type="text"
+                    />
+                    <button>Spara</button>
+            </div>
+                <button>Lägg till artikel</button>
+                <p>Artikeln har lagts till!</p>
         </div>
+
     );
 };
-

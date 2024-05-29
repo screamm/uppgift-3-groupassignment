fa1c4a816ab0323718d087f0f4d13e5ecac90641
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-29 09:25:57 +0200

    changed about to mypages in navbar

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,41 +1,32 @@
-import "../styles/admin.css"
+import "../styles/admin.css";
 
 export const Admin = () => {
-    return(
-        <div className="admin">
-            <h2>Produktlista</h2>
-            <div>
-                    <div>
-                        <h6>Artikelnamn: </h6>
-                        <input
-                            type="text"
-                     />
-                        <label>Pris: </label>
-                        <input
-                           type="text"
-                     />
-                        <button>Spara</button>
-                    </div>
-                    <div>
-                        <h1>Prenumerationer</h1>
-                         <p>Artikelpris:  kr</p>
-                         <button>Redigera Prenumeration</button>
-                    </div>
-            </div>
-            <div>
-                <label>Artikelnamn: </label>
-                    <input
-                         type="text"
-                    />
-                    <label>Pris: </label>
-                    <input
-                     type="text"
-                    />
-                    <button>Spara</button>
-            </div>
-                <button>Lägg till artikel</button>
-                <p>Artikeln har lagts till!</p>
+  return (
+    <div className="admin">
+      <h2>Produktlista</h2>
+      <div>
+        <div>
+          <h6>Artikelnamn: </h6>
+          <input type="text" />
+          <label>Pris: </label>
+          <input type="text" />
+          <button>Spara</button>
         </div>
-
-    );
+        <div>
+          <h1>Prenumerationer</h1>
+          <p>Artikelpris: kr</p>
+          <button>Redigera Prenumeration</button>
+        </div>
+      </div>
+      <div>
+        <label>Artikelnamn: </label>
+        <input type="text" />
+        <label>Pris: </label>
+        <input type="text" />
+        <button>Spara</button>
+      </div>
+      <button>Lägg till artikel</button>
+      <p>Artikeln har lagts till!</p>
+    </div>
+  );
 };

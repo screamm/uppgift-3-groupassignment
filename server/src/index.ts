025f3a1bc3f034931cc63add56237a8ce9017c2d
Screamm <davidrydgren@gmail.com>
2024-05-29 10:35:05 +0200

    Co-authored-by: Jessica Tell <billiswruce@users.noreply.github.com> Co-authored-by: Emspa <Emspa@users.noreply.github.com>rename user to subscriptions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,32 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
-import userRoutes from './routes/user';
+import userRoutes from './routes/subscription';
 import colors from 'colors';
+
 // import dotenv from 'dotenv';
+
+// const cookieSession = require("cookie-session");
+
+// const stripeRouter = require("./resources/stripe/stripe.router");
+// const userRouter = require("./resources/users/users.router");
+// const authRouter = require("./resources/auth/auth.router");
+// const { getProducts } = require("./resources/stripe/stripe.controller");
+
+
 
 // dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//OM VI BEHÖVER CORS
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 // Middleware för att parsa JSON
 app.use(express.json());
@@ -25,8 +43,25 @@ app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
+
+
+// app.use(
+//   cookieSession({
+//     secret: "hemligt",
+//     maxAge: 1000 * 60 * 60 * 24 * 7,
+//   })
+// );
+// app.use("/payments", stripeRouter);
+// app.use("/users", userRouter);
+// app.use("/auth", authRouter);
+// app.get("/products", getProducts);
+
+
+
 // Använda userRoutes för alla rutter under '/api/user'
 app.use('/api/user', userRoutes);
+
+
 
 app.listen(port, () => {
   console.log(colors.rainbow(`Server is running on http://localhost:${port}`));

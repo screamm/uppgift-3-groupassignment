77d89936b2bd5609a7dae2bc373d080e3051daa8
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-30 15:08:03 +0200

    what is happening

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,23 +6,17 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import authRouter from './routes/auth.router';
 import session from 'express-session';
- 
- 
 dotenv.config();
- 
 const app = express();
 const port = process.env.PORT || 3000;
- 
 app.use(express.json());
 app.use(cors());
- 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'defaultsecret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
+secret: process.env.SESSION_SECRET || 'defaultsecret',
+resave: false,
+saveUninitialized: false,
+cookie: { secure: false }
 }));
- 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/grupp5')
   .then(() => console.log(colors.yellow('MongoDB connected')))
   .catch(err => console.log(colors.red(`MongoDB connection error: ${err}`)));

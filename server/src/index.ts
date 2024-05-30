419e37e6799c7deb4e64a59b12a4de9a869f7f4b
Jessica Tell <hello.jessicatell@gmail.com>
2024-05-30 15:10:26 +0200

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,47 +18,34 @@ saveUninitialized: false,
 cookie: { secure: false }
 }));
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/grupp5')
-  .then(() => console.log(colors.yellow('MongoDB connected')))
-  .catch(err => console.log(colors.red(`MongoDB connection error: ${err}`)));
- 
+.then(() => console.log(colors.yellow('MongoDB connected')))
+.catch(err => console.log(colors.red(`MongoDB connection error: ${err}`)));
+
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+   res.send('Hello, TypeScript with Express!');
 });
- 
 app.use('/subscription', subscriptionRouter);
 app.use('/auth', authRouter);
- 
- 
 app.listen(port, () => {
-  console.log(colors.rainbow(`Server is running on http://localhost:${port}`));
+console.log(colors.rainbow(`Server is running on http://localhost:${port}`));
 });
- 
- 
- 
- 
- 
+
+
 // import express from 'express';
 // import mongoose from 'mongoose';
 // import userRoutes from './routes/subscription.router';
 // import colors from 'colors';
 // import subscriptionRouter from './routes/subscription.router';
- 
 // // import dotenv from 'dotenv';
- 
 // // const cookieSession = require("cookie-session");
- 
 // // const stripeRouter = require("./resources/stripe/stripe.router");
 // // const userRouter = require("./resources/users/users.router");
 // // const authRouter = require("./resources/auth/auth.router");
 // // const { getProducts } = require("./resources/stripe/stripe.controller");
 // const articleRoutes = require('./routes/articles.router');
- 
- 
 // // dotenv.config();
- 
 // const app = express();
 // const port = process.env.PORT || 3000;
- 
 // //OM VI BEHÖVER CORS
 // // app.use(
 // //   cors({
@@ -66,10 +53,8 @@ app.listen(port, () => {
 // //     credentials: true,
 // //   })
 // // );
- 
 // // Middleware för att parsa JSON
 // app.use(express.json());
- 
 // // Anslut till MongoDB
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/grupp5', {
 //   // useNewUrlParser: true,
@@ -90,13 +75,8 @@ app.listen(port, () => {
 // // app.use("/users", userRouter);
 // // app.use("/auth", authRouter);
 // // app.get("/products", getProducts);
- 
 // app.use('/subscription', subscriptionRouter);
 // // app.use('/api/articles', articleRoutes)
- 
- 
- 
- 
 // app.listen(port, () => {
 //   console.log(colors.rainbow(`Server is running on http://localhost:${port}`));
 // });

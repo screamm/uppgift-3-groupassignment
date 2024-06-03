b4eabbf5a515afbc1aea93c3df1b08e3e2bb11ef
Jessica Tell <hello.jessicatell@gmail.com>
2024-06-03 14:18:50 +0200

    contact page

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,7 +1,20 @@
+import "../styles/contact.css";
+import img3 from "../img/lvl3.png";
+
 export const Contact = () => {
   return (
-    <div>
-      <h1>Kontaktuppgifter Tjoho ring oss</h1>
+    <div className="contact-container">
+      <h1>Contact</h1>
+      <p>123-456-789</p>
+      <p>alpacaforlife@alpacanews.com</p>
+      <h2>About Alpaca News</h2>
+      <p>
+        Alpaca News is your source for the latest in <br />
+        alpaca-related news, articles, and events. <br />
+        We strive to keep you informed about everything <br />
+        happening in the world of alpacas!
+      </p>
+      <img src={img3} alt="Product Image" style={{ width: "300px" }} />
     </div>
   );
 };

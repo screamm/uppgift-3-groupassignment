52cdad08ecda59ead0d91ff53370dc51f15177f7
Jessica Tell <hello.jessicatell@gmail.com>
2024-06-03 13:48:21 +0200

    -

--- client/src/models/Article.ts
+++ client/src/models/Article.ts
@@ -1,7 +1,7 @@
-import { Key, ReactElement, ReactNode, ReactPortal, JSXElementConstructor } from "react";
+import { Key } from "react";
 
 export interface IProduct {
   id: Key | null | undefined;
-  name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined;
+  name: string;
   price: number;
 }

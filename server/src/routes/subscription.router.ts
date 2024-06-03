08eba232d6724403c6b0f851d4846794e44793d8
Jessica Tell <hello.jessicatell@gmail.com>
2024-06-03 09:54:01 +0200

    -

--- server/src/routes/subscription.router.ts
+++ server/src/routes/subscription.router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getUserSubscription, updateUserSubscription } from '../controllers/subscription.controllers';
-import { getUserSubscription, updateUserSubscription } from '../controllers/user.controllers';
+// import { getUserSubscription, updateUserSubscription } from '../controllers/user.controllers';
 
 const subscriptionRouter = express.Router();
 

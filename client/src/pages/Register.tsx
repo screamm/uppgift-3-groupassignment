bde6214d7d102140ad6a72e0b2df00f6790a202c
Emili Spasovska <emilis95@hotmail.com>
2024-06-04 17:17:22 +0200

    styled registration

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,11 @@
+/** @format */
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { registerUser } from "../services/api";
 import { User } from "../models/User";
+import "../styles/Auth.css"; 
+import alpaca from "../img/alp.png"
 
 export const Register = () => {
   const [formData, setFormData] = useState<User>({
@@ -52,70 +56,69 @@ export const Register = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      {successMessage && (
-        <p style={{ color: "green" }}>
-          {successMessage} <Link to="/login">Login here</Link>
-        </p>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>First Name:</label>
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Role:</label>
-          <select name="role" value={formData.role} onChange={handleChange}>
-            <option value="user">User</option>
-            <option value="admin">Admin</option>
-          </select>
-        </div>
-        <div>
-          <label>
+    <div className="container">
+      <div className="login-container">
+      <img className="alpaca-img" src={alpaca} alt="alpaca" />
+
+        <h1>Register</h1>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        {successMessage && (
+          <p style={{ color: "green" }}>
+            {successMessage} <Link to="/login">Login here</Link>
+          </p>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="checkbox-group">
             <input
               type="checkbox"
               checked={acceptTerms}
               onChange={handleCheckboxChange}
             />
-            I accept the terms and conditions for storing personal data.
-          </label>
-        </div>
-        <button type="submit">Register</button>
-      </form>
+            <p>
+              I accept the terms and conditions for storing personal data.
+            </p>
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </div>
   );
 };
+
+export default Register;

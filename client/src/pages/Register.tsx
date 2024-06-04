39cb0a43d95274c1464de9920d5d0c0d3e438c8d
Jessica Tell <hello.jessicatell@gmail.com>
2024-06-04 13:17:50 +0200

    closing tag label

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -105,52 +105,52 @@ export const Register = () => {
           </select>
         </div>
         <div>
+          <label>Email:</label>
+          <input
+            type="checkbox"
+            checked={acceptTerms}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
           <label>
             <input
               type="checkbox"
               checked={acceptTerms}
               onChange={handleCheckboxChange}
             />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>First Name:</label>
-            <input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>Last Name:</label>
-            <input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                checked={acceptTerms}
-                onChange={handleCheckboxChange}
-              />
-              I accept the terms and conditions for storing personal data.
-            </label>
-          </div>
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    );
-  };
+            I accept the terms and conditions for storing personal data.
+          </label>
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};

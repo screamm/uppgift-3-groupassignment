1aa94680dda9cf011aefbc4273ef4e1c94da7923
fabian0270 <fabianolsson28@gmail.com>
2024-06-05 04:30:22 +0200

    Added subscriptions and articles with designated levels.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,32 +1,141 @@
 import "../styles/admin.css";
+import React, { useState, useEffect } from "react";
+import { IProduct } from "../models/Article";
 
 export const Admin = () => {
-  return (
-    <div className="admin">
-      <h2>Produktlista</h2>
-      <div>
-        <div>
-          <h6>Artikelnamn: </h6>
-          <input type="text" />
-          <label>Pris: </label>
-          <input type="text" />
-          <button>Spara</button>
+    const [articles, setArticles] = useState<IProduct[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [selectedArticle, setSelectedArticle] = useState<IProduct | null>(null);
+    const [contentPages, setContentPages] = useState<{ name: string; requiredLevel: number }[]>([]);
+    const [newPageName, setNewPageName] = useState<string>('');
+    const [selectedSubscription, setSelectedSubscription] = useState<number>(1); 
+    const subscriptions = ["Alpaca Basic", "Alpaca Insight", "Alpaca Elite"]; // Vilka nivåer man kan välja mellan till innehållsidorna.
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("http://localhost:3000/articles/products")
+            .then((response) => response.json())
+            .then((data) => {
+                setArticles(data.reverse());
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching articles:", error);
+                setIsLoading(false);
+            });
+
+        fetch("http://localhost:3000/content/pages")
+            .then((response) => response.json())
+            .then((data) => {
+                setContentPages(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching content pages:", error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    const handleEditClick = (article: IProduct) => {
+        setSelectedArticle(article);
+    }
+
+    const handleSaveClick = async () => {
+        // Implementera uppdatering av prenumeration här
+    }
+
+    const handleAddPageClick = async () => {
+        try {
+            const newPageData = {
+                name: newPageName,
+                requiredLevel: selectedSubscription // Använd vald prenumeration nivå
+            };
+
+            const response = await fetch(`http://localhost:3000/content/pages`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newPageData)
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to add content page: ${response.statusText}`);
+            }
+
+            const updatedPagesResponse = await fetch("http://localhost:3000/content/pages");
+            const updatedPagesData = await updatedPagesResponse.json();
+            setContentPages(updatedPagesData);
+
+            setNewPageName('');
+
+            console.log("Sidans innehåll har lagts till!");
+        } catch (error) {
+            console.error("Error adding content page:", error);
+        }
+    };
+
+    return (
+        <div className="admin">
+            <h2>Prenumerationslista</h2>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="adminSubscriptionList">
+                    {articles.map((article, index) => (
+                        <div key={index} className="subscription">
+                            {selectedArticle && selectedArticle._id === article._id ? (
+                                <div>
+                                    <h6 className="subscriptionName">Prenumerationsnamn: </h6>
+                                    <input
+                                        type="text"
+                                        value={selectedArticle.name}
+                                        onChange={e => setSelectedArticle({...selectedArticle, name: e.target.value})}
+                                    />
+                                    <label>Pris: </label>
+                                    <input
+                                        type="text"
+                                        value={selectedArticle.price}
+                                        onChange={e => setSelectedArticle({...selectedArticle, price: parseFloat(e.target.value)})}
+                                    />
+                                    <button onClick={handleSaveClick}>Spara</button>
+                                </div>
+                            ) : (
+                                <div>
+                                    <h1>{article.name}</h1>
+                                    <p>prenumerationspris: {article.price} kr</p>
+                                    <button onClick={() => handleEditClick(article)} className="editArticle">Redigera prenumerationen</button>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            <h2>Innehållssidor</h2>
+            <div className="adminContentPages">
+                {contentPages.map((page, index) => (
+                    <div key={index} className="contentPage">
+                        <h3>{page.name}</h3>
+                        <p>Krävd nivå: {subscriptions[page.requiredLevel - 1]}</p> {/* Visa prenumerationen baserat på krävd nivå */}
+                    </div>
+                ))}
+                <div className="addContentPageForm">
+                    <label>Sidans namn: </label>
+                    <input
+                        type="text"
+                        value={newPageName}
+                        onChange={e => setNewPageName(e.target.value)}
+                    />
+                    <label>Välj prenumeration: </label>
+                    <select value={selectedSubscription} onChange={(e) => setSelectedSubscription(parseInt(e.target.value))}>
+                        {subscriptions.map((subscription, index) => (
+                            <option key={index} value={index + 1}>{subscription}</option>
+                        ))}
+                    </select>
+                    <button onClick={handleAddPageClick}>Lägg till sida</button>
+                </div>
+            </div>
         </div>
-        <div>
-          <h1>Prenumerationer</h1>
-          <p>Artikelpris: kr</p>
-          <button>Redigera Prenumeration</button>
-        </div>
-      </div>
-      <div>
-        <label>Artikelnamn: </label>
-        <input type="text" />
-        <label>Pris: </label>
-        <input type="text" />
-        <button>Spara</button>
-      </div>
-      <button>Lägg till artikel</button>
-      <p>Artikeln har lagts till!</p>
-    </div>
-  );
+    );
 };

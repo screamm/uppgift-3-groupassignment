57d655a7c391f71258d434c33f1cb00c111fa170
screamm <davidrydgren@gmail.com>
2024-06-11 10:38:16 +0200

    login problem kvarstår

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -31,7 +31,7 @@ export const Login = () => {
       console.log("Login successful:", response.data);
 
       setErrorMessage("");
-      login(response.data, response.data.sessionId); // Skicka sessionId när användaren loggar in
+      login(response.data, response.data.stripeSessionId); // Skicka sessionId när användaren loggar in
       console.log("User logged in:", response.data);
 
       navigate("/mypages");
